390f041564aaeaa20ebcaca8d3e630a67adc6c1d
Ggyumalang <khg2154@naver.com>
2023-02-23 18:13:39 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/healbits/HealbitsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.healbits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealbitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealbitsApplication.class, args);
+	}
+
+}
